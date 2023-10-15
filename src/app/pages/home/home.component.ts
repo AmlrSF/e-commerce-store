c5f7ac775814f956cec40c5e9853a77d6d5f5162
softyAmir <theyabcpssouaf2@gmail.com>
2023-10-15 09:40:37 -1100

    add some routing

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  public slides = [
+    {
+      src: '../../../assets/slider/slider0.jpg',
+      title: 'Spring Fashion Extravaganza',
+      subtitle: 'Get Ready for the Season\'s Hottest Styles',
+    },
+    {
+      src: '../../../assets/slider/slider1.jpg',
+      title: 'Exclusive Discounts Inside',
+      subtitle: 'Shop Now for Unbeatable Savings',
+    },
+    {
+      src: '../../../assets/slider/slider2.jpg',
+      title: 'Elevate Your Style',
+      subtitle: 'Discover Premium Fashion Choices',
+    },
+    {
+      src: '../../../assets/slider/slider3.jpg',
+      title: 'New Arrivals Every Week',
+      subtitle: 'Stay Ahead of the Fashion Curve',
+    },
+  ];
+  
+
   
 }
