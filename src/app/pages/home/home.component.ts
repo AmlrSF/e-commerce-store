d6d11e4d28cdff6bdfaa9a37c6dcab7fdd7e4df1
softyAmir <theyabcpssouaf2@gmail.com>
2023-11-04 07:02:06 -1100

    price func

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -48,4 +48,24 @@ export class HomeComponent implements OnInit {
     )
   }
 
+  public formatPrice(price:any) {
+    if (typeof price === 'string') {
+      // If the price is a string, check for the presence of '$' symbol.
+      if (price.includes('$')) {
+        // If '$' is on the left, move it to the right.
+        return price.replace('$', '') + '$';
+      } else {
+        // If '$' is not present, add it to the right.
+        return price + '$';
+      }
+    } else if (typeof price === 'number') {
+      // If the price is a number, convert it to a string and add '$' on the right.
+      return price.toString() + '$';
+    } else {
+      // Handle other cases, e.g., when the data is not a string or a number.
+      return 'N/A';
+    }
+  }
+  
+
 }
