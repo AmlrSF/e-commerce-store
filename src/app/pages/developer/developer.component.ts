ca4e52615d934cfbeebce1ce9158add888dfdef4
softyAmir <theyabcpssouaf2@gmail.com>
2023-11-13 00:54:43 -1100

    upadeate deleoper page

--- src/app/pages/developer/developer.component.ts
+++ src/app/pages/developer/developer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-developer',
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./developer.component.css']
 })
 export class DeveloperComponent {
+  copyIcon = 'fa-copy';
 
+  copyToClipboard(apiUrl :string) {
+    
+    const tempInput = document.createElement('textarea');
+    tempInput.value = apiUrl;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand('copy');
+    document.body.removeChild(tempInput);
+
+    
+    this.copyIcon = 'fa-check';
+
+    
+    setTimeout(() => {
+      this.copyIcon = 'fa-copy';
+    }, 3000);
+  }
 }
